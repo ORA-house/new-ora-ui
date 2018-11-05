c07f5b602564557887defba6320361ea66388ba7
sk43 <elizabethkwan@outlook.com>
2018-11-05 15:14:05 +0400

    phase1

--- libs/animation.js
+++ libs/animation.js
@@ -1,4 +1,8 @@
-$("#menu").click(function() {
-    $('.menu-container').toggleClass('active');
-    $('#menu').toggleClass('active');
-  });
+$("#menu").click(function () {
+  $('.menu-container').toggleClass('active');
+  $('#menu').toggleClass('active');
+});
+
+$('.menu-container button').click(function () {
+
+});
