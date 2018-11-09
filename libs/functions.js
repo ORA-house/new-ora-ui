8a493cf3592c9c70382b395be8a52034ab4a5808
unknown <emardc15@gmail.com>
2018-11-09 22:21:22 +0400

    UI for AC control done

--- libs/functions.js
+++ libs/functions.js
@@ -1,17 +1,27 @@
-function incTemp() {
-    var element = document.getElementById('setTemp');
-    var num = element.innerHTML;
-    num++;
-    element.innerHTML = num;
-}
+const AC_MODES = ["Auto", "Cool", "Heat", "Fan"];
 
-function decTemp() {
-    var element = document.getElementById('setTemp');
-    var num = element.innerHTML;
-    num--;
-    element.innerHTML = num;
-}
+$('.minus-ac-temp').click(function () {
+    if (parseInt($('#setTemp').text()) > 5) {
+        $('#setTemp').text(parseInt($('#setTemp').text()) - 1);
+    }
+});
 
-function goTo(link) {
-    window.location.href = link;
-}
+$('.add-ac-temp').click(function () {
+    if (parseInt($('#setTemp').text()) < 30) {
+        $('#setTemp').text(parseInt($('#setTemp').text()) + 1);
+    }
+});
+
+$('.previous-ac-mode').click(function () {
+    let mode = $('#acMode').text();
+    if (AC_MODES.indexOf(mode) > 0) {
+        $('#acMode').text(AC_MODES[AC_MODES.indexOf(mode) - 1]);
+    }
+});
+
+$('.next-ac-mode').click(function () {
+    let mode = $('#acMode').text();
+    if (AC_MODES.indexOf(mode) < 3) {
+        $('#acMode').text(AC_MODES[AC_MODES.indexOf(mode) + 1]);
+    }
+});
---
+++ libs/navigation.js
@@ -0,0 +1,27 @@
+$('#control-nav').click(function () {
+    window.location.replace('control.html');
+});
+
+$('#mBed-op').click(function () {
+    window.location.replace('rooms/master-bedroom.html');
+});
+
+$('#cBed-op').click(function () {
+    window.location.replace('rooms/children-bedroom.html');
+});
+
+$('#kitchen-op').click(function () {
+    window.location.replace('rooms/kitchen.html');
+});
+
+$('#liv-op').click(function () {
+    window.location.replace('rooms/living-room.html');
+});
+
+$('#deck-op').click(function () {
+    window.location.replace('rooms/deck.html');
+});
+
+$('#car-op').click(function () {
+    window.location.replace('rooms/car-parking.html');
+});
