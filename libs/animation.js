96918278d0aa7fe820a5e602fb8358a83fb1958e
sk43 <elizabethkwan@outlook.com>
2018-11-10 19:34:35 +0400

    face detection with welcome back

--- libs/animation.js
+++ libs/animation.js
@@ -2,3 +2,8 @@ $('#menu').click(function () {
   $('.menu-container').toggleClass('active');
   $('#menu').toggleClass('active');
 });
+
+$('#login').click(function() {
+  detectFace();
+  $('#home-vid').toggleClass('login-bg');
+});
