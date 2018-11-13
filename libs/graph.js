a29b3978d529c6950ec16b2e0a8657d1fec4b6b6
Sreenithi <19sreenithi98@gmail.com>
2018-11-13 12:28:17 +0400

    updated graph.js to use the same file for all stats pages

--- libs/graph.js
+++ libs/graph.js
@@ -11,242 +11,337 @@ window.chartColors = {
 window.randomScalingFactor = function () {
     return (Math.random() > 0.5 ? 1.0 : 2.0) * Math.random() * 100;
 };
-var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-var configCarbon = {
-    type: 'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Carbon',
-            borderColor: window.chartColors.red,
-            backgroundColor: window.chartColors.red,
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ],
-        }]
-    },
-    options: {
-        responsive: true,
-        tooltips: {
-            mode: 'index',
-        },
-        hover: {
-            mode: 'index'
-        },
-        scales: {
-            xAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Month'
-                }
-            }],
-            yAxes: [{
-                stacked: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Value'
-                }
-            }]
-        }
-    }
-};
-
-var configElectrical = {
-    type: 'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Electricity Consumption',
-            borderColor: window.chartColors.green,
-            backgroundColor: window.chartColors.green,
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ],
-        }]
-    },
-    options: {
-        responsive: true,
-        tooltips: {
-            mode: 'index',
-        },
-        hover: {
-            mode: 'index'
-        },
-        scales: {
-            xAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Month'
-                }
-            }],
-            yAxes: [{
-                stacked: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Value'
-                }
-            }]
-        }
-    }
-};
-
-var configHumidity = {
-    type: 'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Humidity Level',
-            borderColor: window.chartColors.grey,
-            backgroundColor: window.chartColors.grey,
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ],
-        }]
-    },
-    options: {
-        responsive: true,
-        tooltips: {
-            mode: 'index',
-        },
-        hover: {
-            mode: 'index'
-        },
-        scales: {
-            xAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Month'
-                }
-            }],
-            yAxes: [{
-                stacked: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Value'
-                }
-            }]
-        }
-    }
-};
-
-var configTemp = {
-    type: 'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Temperature',
-            borderColor: window.chartColors.yellow,
-            backgroundColor: window.chartColors.yellow,
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ],
-        }]
-    },
-    options: {
-        responsive: true,
-        tooltips: {
-            mode: 'index',
-        },
-        hover: {
-            mode: 'index'
-        },
-        scales: {
-            xAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Month'
-                }
-            }],
-            yAxes: [{
-                stacked: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Value'
-                }
-            }]
-        }
-    }
-};
-
-var configWater = {
-    type: 'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Water Consumption',
-            borderColor: window.chartColors.blue,
-            backgroundColor: window.chartColors.blue,
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ],
-        }]
-    },
-    options: {
-        responsive: true,
-        tooltips: {
-            mode: 'index',
-        },
-        hover: {
-            mode: 'index'
-        },
-        scales: {
-            xAxes: [{
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Month'
-                }
-            }],
-            yAxes: [{
-                stacked: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Value'
-                }
-            }]
-        }
-    }
-};
 
 window.onload = function () {
-    var ctxCarbon = document.getElementById('carbonChart').getContext('2d');
-    var ctxElectric = document.getElementById('electricityChart').getContext('2d');
-    var ctxHumidity = document.getElementById('humidityChart').getContext('2d');
-    var ctxTemp = document.getElementById('tempChart').getContext('2d');
-    var ctxWater = document.getElementById('waterChart').getContext('2d');
-    
-    window.myLine = new Chart(ctxCarbon, configCarbon);
-    window.myLine = new Chart(ctxElectric, configElectrical);    
-    window.myLine = new Chart(ctxHumidity, configHumidity);
-    window.myLine = new Chart(ctxTemp, configTemp);
-    window.myLine = new Chart(ctxWater, configWater);
+    var carbonElementDaily = document.getElementById('carbonChartDaily');
+    var electricityElementDaily = document.getElementById('electricityChartDaily');
+    var humidityElementDaily = document.getElementById('humidityChartDaily');
+    var tempElementDaily = document.getElementById('tempChartDaily');
+    var waterElementDaily = document.getElementById('waterChartDaily');
+
+    var carbonElementLive = document.getElementById('carbonChartLive');
+    var electricityElementLive = document.getElementById('electricityChartLive');
+    var humidityElementLive = document.getElementById('humidityChartLive');
+    var tempElementLive = document.getElementById('tempChartLive');
+    var waterElementLive = document.getElementById('waterChartLive');
+
+    //**************************************************************/
+    //                      Daily Charts
+    /***************************************************************/
+        
+    if(carbonElementDaily != null)
+    {
+        var ctxCarbonDaily = carbonElementDaily.getContext('2d');
+
+        var configCarbonDaily = {
+            type: 'line',
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'Carbon',
+                    borderColor: window.chartColors.red,
+                    backgroundColor: window.chartColors.red,
+                    data: [
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor()
+                    ],
+                }]
+            },
+            options: {
+                responsive: true,
+                tooltips: {
+                    mode: 'index',
+                },
+                hover: {
+                    mode: 'index'
+                },
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Month'
+                        }
+                    }],
+                    yAxes: [{
+                        stacked: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value'
+                        }
+                    }]
+                }
+            }
+        };
+
+        window.myLine = new Chart(ctxCarbonDaily, configCarbonDaily);
+    }
+
+    if(electricityElementDaily != null)
+    {
+        var ctxElectricityDaily = electricityElementDaily.getContext('2d');
+
+        var configElectricityDaily = {
+            type: 'line',
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'Electricity Consumption',
+                    borderColor: window.chartColors.green,
+                    backgroundColor: window.chartColors.green,
+                    data: [
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor()
+                    ],
+                }]
+            },
+            options: {
+                responsive: true,
+                tooltips: {
+                    mode: 'index',
+                },
+                hover: {
+                    mode: 'index'
+                },
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Month'
+                        }
+                    }],
+                    yAxes: [{
+                        stacked: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value'
+                        }
+                    }]
+                }
+            }
+        };
+
+        window.myLine = new Chart(ctxElectricityDaily, configElectricityDaily);
+    }
+
+    if(humidityElementDaily != null)
+    {
+        var ctxHumidityDaily = humidityElementDaily.getContext('2d');
+
+        var configHumidityDaily = {
+            type: 'line',
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'Humidity Level',
+                    borderColor: window.chartColors.grey,
+                    backgroundColor: window.chartColors.grey,
+                    data: [
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor()
+                    ],
+                }]
+            },
+            options: {
+                responsive: true,
+                tooltips: {
+                    mode: 'index',
+                },
+                hover: {
+                    mode: 'index'
+                },
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Month'
+                        }
+                    }],
+                    yAxes: [{
+                        stacked: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value'
+                        }
+                    }]
+                }
+            }
+        };
+
+        window.myLine = new Chart(ctxHumidityDaily, configHumidityDaily);
+    }
+
+    if(tempElementDaily != null)
+    {
+        var ctxTempDaily = tempElementDaily.getContext('2d');
+
+        var configTempDaily = {
+            type: 'line',
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'Temperature',
+                    borderColor: window.chartColors.yellow,
+                    backgroundColor: window.chartColors.yellow,
+                    data: [
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor()
+                    ],
+                }]
+            },
+            options: {
+                responsive: true,
+                tooltips: {
+                    mode: 'index',
+                },
+                hover: {
+                    mode: 'index'
+                },
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Month'
+                        }
+                    }],
+                    yAxes: [{
+                        stacked: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value'
+                        }
+                    }]
+                }
+            }
+        };
+
+        window.myLine = new Chart(ctxTempDaily, configTempDaily);
+    }
+
+    if(waterElementDaily != null)
+    {
+        var ctxWaterDaily = waterElementDaily.getContext('2d');
+
+        var configWaterDaily = {
+            type: 'line',
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'Water Consumption',
+                    borderColor: window.chartColors.blue,
+                    backgroundColor: window.chartColors.blue,
+                    data: [
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor()
+                    ],
+                }]
+            },
+            options: {
+                responsive: true,
+                tooltips: {
+                    mode: 'index',
+                },
+                hover: {
+                    mode: 'index'
+                },
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Month'
+                        }
+                    }],
+                    yAxes: [{
+                        stacked: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value'
+                        }
+                    }]
+                }
+            }
+        };
+
+        window.myLine = new Chart(ctxWaterDaily, configWaterDaily);
+    }
+
+    /***************************************************************/
+    //                      Live Charts
+    /***************************************************************/
+
+    if(carbonElementLive != null)
+    {
+        var ctxCarbonLive = carbonElementLive.getContext('2d');
+
+        var configCarbonLive = {
+            type: 'line',
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'Water Consumption',
+                    borderColor: window.chartColors.purple,
+                    backgroundColor: window.chartColors.purple,
+                    data: [
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor(),
+                        randomScalingFactor()
+                    ],
+                }]
+            },
+            options: {
+                responsive: true,
+                tooltips: {
+                    mode: 'index',
+                },
+                hover: {
+                    mode: 'index'
+                },
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Month'
+                        }
+                    }],
+                    yAxes: [{
+                        stacked: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Value'
+                        }
+                    }]
+                }
+            }
+        };
+        
+        window.myLine = new Chart(ctxCarbonLive, configCarbonLive);
+    }
 };
